d077374d41c6c876be13bc2197f837898ea95adc
YousefAttia49 <yousefattia286@gmail.com>
2024-12-21 23:11:49 +0200

    Maguc_Func.py

--- Maguc_Func.py
+++ Maguc_Func.py
@@ -1,24 +1,33 @@
-class Vichle:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
+class Customer:
+    def __init__(self,name,balance):
+        self.name=name
+        self.balance=balance
+    def __str__(self):
+        return f'Name :  {self.name} , Balance : {self.balance}'
+    
+    def __eq__(self,other):
+        return self.balance== other.balance
+    def __gt__(self,other):
+        return self.balance>other.balance
+    def __lt__(self,other):
+        return self.balance<other.balance
+    
+    def __add__(self,other):
+        return self.balance + other.balance
+    
+    def __sub__(self,other):
+        return self.balance - other.balance
+    
+    
+    
+customer1=Customer('yousef', 20000)
+customer2=Customer('hossam', 50000)
 
-    def info(self):
-        print(f'the model is {self.model}')
-        print(f'the brand is {self.brand}')
+print(customer1 == customer2)    
+print(customer1 > customer2)    
+print(customer1 < customer2)    
+print(customer1 + customer2)    
+print(customer1 - customer2)    
 
-
-class Car(Vichle):
-    def __init__(self, brand, model, type):
-        super().__init__(brand, model)
-        self.type = type
-
-    def info(self):
-        super().__init__()
-        print(f'the type is {self.type}')
-
-
-yousef = Car('Mosteng', 1999, 'Gradw 7',)
-yousef.info()
-
-
+    
+    
