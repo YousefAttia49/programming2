5a991827a1b6259796b1dbee357f37a95546b544
YousefAttia49 <yousefattia286@gmail.com>
2024-12-22 12:57:31 +0200

    Add files via upload

---
+++ DocString.py
@@ -0,0 +1,21 @@
+def add(x: int, y: int) -> int:
+    """
+    Calculate the sum of two numbers.
+
+    Parameters
+    ----------
+    x : int
+        The first integer number.
+    y : int
+        The second integer number.
+
+    Returns
+    -------
+    int
+        The sum of x and y.
+    """
+    return x + y
+
+print(add(5, 2))  
+print(add.__doc__) 
+
